16f537953196d98a0637c2ebea932e1b039a161f
aegikian <aegikian@saber.games>
2024-03-11 17:47:27 +0400

    new tools

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -5,20 +5,70 @@ export const Canvas = () => {
   const [color, setColor] = useState("black");
   const [size, setSize] = useState("1");
   const [cnvs, setCnvs] = useState<any>();
+  const [hand, setHand] = useState<"square" | "circle" | "pencil" | "line">(
+    "pencil"
+  );
+  const [shftD, setShiftD] = useState(false);
   useEffect(() => {
     setCnvs(createCanvas());
+    window.addEventListener("keydown", (e) => handleShift(e, "down"));
+    window.addEventListener("keyup", (e) => handleShift(e, "up"));
   }, []);
   const handleDrawer = (e: React.MouseEvent) => {
     cnvs.drawer(e);
   };
+  const handleShift = (e: KeyboardEvent, mode: string) => {
+    if (mode === "down" && e.key === "Shift") setShiftD(true);
+    if (mode === "up" && e.key === "Shift") setShiftD(false);
+  };
   const handleDraw = (e: React.MouseEvent) => {
-    if (e.buttons == 1) {
-      cnvs.stroker({ x: e.clientX, y: e.clientY }, color, size);
+    switch (hand) {
+      case "pencil":
+        if (e.buttons == 1) {
+          cnvs.stroker({ x: e.clientX, y: e.clientY }, color, size);
+        }
+        break;
+      case "circle":
+        if (e.buttons === 1) {
+          cnvs.circler({ x: e.clientX, y: e.clientY }, color, +size);
+        } else {
+          cnvs.circler({ x: e.clientX, y: e.clientY }, color, +size, true);
+        }
+        break;
+      case "square":
+        if (e.buttons == 1) {
+          cnvs.squarer(
+            color,
+            size,
+            { x: e.clientX, y: e.clientY },
+            { x: e.clientX, y: e.clientY }
+          );
+        } else if (e.buttons == 0) {
+          cnvs.squarer(color, size, null, { x: e.clientX, y: e.clientY }, true);
+        }
+        break;
+      case "line":
+        if (e.buttons == 1) {
+          cnvs.liner({ x: e.clientX, y: e.clientY }, null, color, size);
+        } else if (e.buttons == 0) {
+          cnvs.liner(
+            null,
+            { x: e.clientX, y: e.clientY },
+            color,
+            size,
+            true,
+            shftD
+          );
+        }
+        break;
+      default:
+        break;
     }
   };
   return (
     <div className="canvas_all">
       <div className="canvas_panel">
+        <span>{shftD ? "shift is pushed" : ""}</span>
         <label htmlFor="color_picker">Color:</label>
         <input
           id="color_picker"
@@ -33,6 +83,30 @@ export const Canvas = () => {
           value={size}
           onChange={(e) => setSize(e.target.value)}
         />
+        <button
+          onClick={() => setHand("circle")}
+          className={hand === "circle" ? "pen_active pen" : "pen"}
+        >
+          circle
+        </button>
+        <button
+          onClick={() => setHand("square")}
+          className={hand === "square" ? "pen_active pen" : "pen"}
+        >
+          square
+        </button>
+        <button
+          onClick={() => setHand("pencil")}
+          className={hand === "pencil" ? "pen_active pen" : "pen"}
+        >
+          pencil
+        </button>
+        <button
+          onClick={() => setHand("line")}
+          className={hand === "line" ? "pen_active pen" : "pen"}
+        >
+          line
+        </button>
       </div>
       <canvas
         width={screen.width - 20}
--- src/components/canvas/canvas_store.tsx
+++ src/components/canvas/canvas_store.tsx
@@ -3,12 +3,13 @@ const createCanvas = () => {
   let ctx = canvas.getContext("2d");
   const cashe: ImageData[] = [];
   const redo_cashe: ImageData[] = [];
+  let startPos: { x: number; y: number } | null = null;
+  let endPos: { x: number; y: number } | null = null;
+  let lastCoords: { x: number; y: number } | null = null;
 
   const drawer = (e: React.MouseEvent) => {
     if (e.buttons === 1) {
       ctx.beginPath();
-      console.log("qweqweqwe");
-
       cashe.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
     }
   };
@@ -25,6 +26,86 @@ const createCanvas = () => {
     ctx.lineWidth = +size;
     ctx.lineTo(x, y);
     ctx.stroke();
+  };
+
+  const squarer = (
+    color: string,
+    size: number,
+    start?: { x: number; y: number },
+    end?: { x: number; y: number },
+    draw?: boolean
+  ) => {
+    const rect = canvas.getBoundingClientRect();
+    if (startPos == null && start) {
+      startPos = { x: start.x - rect.left, y: start.y - rect.top };
+    }
+    endPos = { x: end.x - rect.x, y: end.y - rect.y };
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+
+    if (startPos && endPos && draw) {
+      const width = endPos.x - startPos.x;
+      const height = endPos.y - startPos.y;
+      ctx.strokeRect(startPos.x, startPos.y, width, height);
+      startPos = null;
+      endPos = null;
+    }
+  };
+  const circler = (
+    start: { x: number; y: number },
+    color: string,
+    size: number,
+    end?: boolean
+  ) => {
+    const rect = canvas.getBoundingClientRect();
+    if (startPos == null && start) {
+      startPos = { x: start.x - rect.left, y: start.y - rect.top };
+    }
+    if (startPos && start.x && start.y) {
+      const radius = Math.sqrt(
+        Math.pow(startPos.x - (start ? start.x - rect.left : 0), 2) +
+          Math.pow(startPos.y - (start ? start.y - rect.top : 0), 2)
+      );
+
+      const centerX = startPos.x;
+      const centerY = startPos.y;
+
+      ctx.strokeStyle = color;
+      ctx.lineWidth = size;
+      if (end && startPos.x && startPos.y) {
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+        startPos = null;
+      }
+    }
+  };
+
+  const liner = (
+    start: { x: number; y: number },
+    end: { x: number; y: number } | null,
+    color: string,
+    size: string,
+    isEnd?: boolean,
+    isShift?: boolean
+  ) => {
+    const rect = canvas.getBoundingClientRect();
+    if (!startPos && start)
+      startPos = { x: start.x - rect.left, y: start.y - rect.top };
+    if (end !== null) {
+      endPos = { x: end.x - rect.left, y: end.y - rect.top };
+    }
+    ctx.strokeStyle = color;
+    ctx.lineWidth = +size;
+    if (end && isEnd && startPos && endPos) {
+      ctx.moveTo(startPos.x, startPos.y);
+      ctx.lineTo(endPos.x, endPos.y);
+      ctx.stroke();
+      if (!isShift) {
+        startPos = null;
+        endPos = null;
+      }
+    }
   };
 
   const clear = () => {
@@ -50,7 +131,17 @@ const createCanvas = () => {
       }
     }
   };
-  return { canvas, ctx, drawer, stroker, clear, undo, redo };
+  return {
+    canvas,
+    ctx,
+    drawer,
+    stroker,
+    clear,
+    undo,
+    redo,
+    squarer,
+    circler,
+    liner,
+  };
 };
-
 export default createCanvas;
